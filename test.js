7a8a7897251d5a0efd072ce6778b646ca702501e
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-24 17:04:11 +0800

    init

---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict';
+var test = require('ava');
+var isBinaryPath = require('./');
+
+test(function (t) {
+	t.assert(isBinaryPath('unicorn.png'));
+	t.assert(isBinaryPath('unicorn.zip'));
+	t.assert(!isBinaryPath('unicornzip'));
+	t.assert(!isBinaryPath('unicorn.txt'));
+	t.end();
+});
