df31975fabeaef309d92f37b0b4607e416643765
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-25 13:32:21 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var isBinaryPath = require('./');
 test(function (t) {
 	t.assert(isBinaryPath('unicorn.png'));
 	t.assert(isBinaryPath('unicorn.zip'));
+	t.assert(isBinaryPath('unicorn.ZIP'));
 	t.assert(!isBinaryPath('unicornzip'));
 	t.assert(!isBinaryPath('unicorn.txt'));
 	t.end();
