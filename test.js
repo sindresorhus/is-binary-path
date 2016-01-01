7a60b54a61bfaf8da5b868789dee067093ed3300
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 01:03:55 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
-'use strict';
-var test = require('ava');
-var isBinaryPath = require('./');
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.assert(isBinaryPath('unicorn.png'));
-	t.assert(isBinaryPath('unicorn.zip'));
-	t.assert(isBinaryPath('unicorn.ZIP'));
-	t.assert(!isBinaryPath('unicornzip'));
-	t.assert(!isBinaryPath('unicorn.txt'));
-	t.end();
+test(t => {
+	t.true(m('unicorn.png'));
+	t.true(m('unicorn.zip'));
+	t.true(m('unicorn.ZIP'));
+	t.false(m('unicornzip'));
+	t.false(m('unicorn.txt'));
 });
